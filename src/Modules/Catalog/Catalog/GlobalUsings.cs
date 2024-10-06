05f1e24f287d787ae2c622d79eacec72220a4072
Rahul Sen <rsen253@gmail.com>
2024-10-06 16:16:26 +0530

    Created domain events Raise domain events on product creation and price change

--- src/Modules/Catalog/Catalog/GlobalUsings.cs
+++ src/Modules/Catalog/Catalog/GlobalUsings.cs
@@ -1,3 +1,7 @@
 ﻿global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.AspNetCore.Builder;
+
+global using Shared.DDD;
+global using Catalog.Products.Models;
+global using Catalog.Products.Events;
