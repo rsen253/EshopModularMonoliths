381459ac8957e21c06ccc64cb8090fa3a5539896
Rahul Sen <rsen253@gmail.com>
2024-10-13 19:19:47 +0530

    Added migration and use generic migration method for auto migration

--- src/Modules/Basket/Basket/BasketModule.cs
+++ src/Modules/Basket/Basket/BasketModule.cs
@@ -1,24 +1,22 @@
-﻿
-namespace Basket;
+﻿namespace Basket;
 
 public static class BasketModule
 {
     public static IServiceCollection AddBasketModule(this IServiceCollection services, IConfiguration configuration)
     {
-        // Register your services 
-        //services
-        //    .AddApplicationServices()
-        //    .AddInfrastructureServices(configuration)
-        //    .AddApiServices(configuration);
+        var connectionString = configuration.GetConnectionString("Database");
+        services.AddScoped<ISaveChangesInterceptor, AuditableEntityInterceptor>();
+        services.AddScoped<ISaveChangesInterceptor, DispatchDomainEventsInterceptor>();
+        services.AddDbContext<BasketDbContext>((sp, options) =>
+        {
+            options.AddInterceptors(sp.GetServices<ISaveChangesInterceptor>());
+            options.UseNpgsql(connectionString);
+        });
         return services;
     }
     public static IApplicationBuilder UseBasketModule(this IApplicationBuilder app)
     {
-        // configure http pipeline
-        //app
-        // .UseApplicationServices()
-        // .UseInfrastructureServices(configuration)
-        // .UseApiServices(configuration);
+        app.UseMigration<BasketDbContext>();
         return app;
     }
 }
--- src/Modules/Basket/Basket/GlobalUsings.cs
+++ src/Modules/Basket/Basket/GlobalUsings.cs
@@ -1,5 +1,12 @@
 ﻿global using Microsoft.Extensions.Configuration;
 global using Microsoft.Extensions.DependencyInjection;
 global using Microsoft.AspNetCore.Builder;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+global using Microsoft.EntityFrameworkCore.Diagnostics;
 
 global using Shared.DDD;
+global using Basket.Basket.Models;
+global using Basket.Data;
+global using Shared.Data.Interceptor;
+global using Shared.Data;
