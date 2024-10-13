5290e60a741474f861cffaf927c58fb9ae1cb99b
Rahul Sen <rsen253@gmail.com>
2024-10-13 20:02:20 +0530

    Added all the feature handlers for the basket module

---
+++ src/Modules/Basket/Basket/Basket/Exceptions/BasketNotFoundException.cs
@@ -0,0 +1,6 @@
+﻿namespace Basket.Basket.Exceptions;
+
+public class BasketNotFoundException(string userName)
+    : NotFoundException("ShoppingCart", userName)
+{
+}
--- src/Modules/Basket/Basket/GlobalUsings.cs
+++ src/Modules/Basket/Basket/GlobalUsings.cs
@@ -4,9 +4,15 @@ global using Microsoft.AspNetCore.Builder;
 global using Microsoft.EntityFrameworkCore;
 global using Microsoft.EntityFrameworkCore.Metadata.Builders;
 global using Microsoft.EntityFrameworkCore.Diagnostics;
+global using Basket.Basket.Dtos;
+global using FluentValidation;
+global using Mapster;
 
 global using Shared.DDD;
 global using Basket.Basket.Models;
 global using Basket.Data;
 global using Shared.Data.Interceptor;
 global using Shared.Data;
+global using Shared.CQRS;
+global using Shared.Exceptions;
+global using Basket.Basket.Exceptions;
